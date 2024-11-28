0448edc3e68f97731c3d8fab5034768b7d543e67
divyesh <divyeshkonkani@gmail.com>
2024-11-28 08:52:52 -0500

    Add project files.

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Age: {Age(DateTime.Today.AddYears(-11).AddDays(1),DateTime.Today)} Year(s)");
+
+static int Age(DateTime from, DateTime to, int age = 0)
+{
+    DateTime next=from.AddYears(1).AddDays(-1);
+    if(to<=next) return age;
+
+    return Age(next.AddDays(1),to,++age);
+}
